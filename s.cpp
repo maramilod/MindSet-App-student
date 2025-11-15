53833438acb86430d8867898d878a73fe0373a5e
maramilod <135626011+maramilod@users.noreply.github.com>
2025-11-15 12:08:11 +0200

    Solution for task: 52

---
+++ s.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string input;
+    cin >> input;      // أخذ الإدخال من المستخدم
+    cout << input;     // طباعة نفس الإدخال
+    return 0;
+}
